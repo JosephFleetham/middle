edd70825861e8c887968113106e1dbdd5176b2c1
JosephFleetham <joseph.fleetham@gmail.com>
2017-09-15 17:57:13 +0000

    added images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from "./data.json"
 
 const App = React.createClass({
   getInitialState: function () {
@@ -9,24 +10,22 @@ const App = React.createClass({
         {
           title: 'Practice squat',
           project: 'Gym Chores',
-          id: 1,
-          elapsed: 5456099,
-          runningSince: Date.now(),
+          photo: "https://unsplash.it/500/200/?random=" + Math.random()
         },
         {
           title: 'Bake squash',
           project: 'Kitchen Chores',
-          id: 2,
-          elapsed: 1273998,
-          runningSince: null,
+          photo: "https://unsplash.it/500/200/?random=" + Math.random()
         },
         {
           title: "do shit",
-          project: "shit shit"
+          project: "shit shit",
+          photo: "https://unsplash.it/500/200/?random=" + Math.random()
         },
         {
           title: "more shit",
-          project: "tryign to get rid of shit"
+          project: "tryign to get rid of shit",
+          photo: "https://unsplash.it/500/200/?random=" + +Math.random()
         }
       ],
     };
@@ -50,21 +49,56 @@ const App = React.createClass({
 });
 
 const CardList = React.createClass({
+  // onNewCard: function(index) {
+  //   var newData = this.state.cards.slice(); //copy array
+  //   newData.splice(index, 1); //remove element
+  //   this.setState({cards: newData}); //update state
+  // },
+  // handleNewCard: function (cards) {
+  //   this.onNewCard();
+  // },
+  // componentDidMount: function () {
+  //   this.handleNewCard();
+  // },
+  // this.setState({
+  //   data: update(this.state.data, {$splice: [[index, 1]]})
+  // }),
   render: function () {
     // leanpub-start-insert
     const cards = this.props.cards.map((card) => (
       <Card
-        key={card.id}
-        id={card.id}
         title={card.title}
         project={card.project}
-        elapsed={card.elapsed}
-        runningSince={card.runningSince}
+        photo={card.photo}
       />
     ));
     for (var i=0; i<cards.length; i++) {
       return (
         <div className="ui grid">
+          <div className="two wide column">
+          </div>
+          <div className="five wide column">
+            {cards[i + (Math.floor(Math.random() * cards.length))]}
+          </div>
+          <div className="one wide column">
+          </div>
+          <div className="five wide column">
+            {cards[i + (Math.floor(Math.random() * cards.length))]}
+          </div>
+          <div className="two wide column">
+          </div>
+          <div className="two wide column">
+          </div>
+          <div className="five wide column">
+            {cards[i + (Math.floor(Math.random() * cards.length))]}
+          </div>
+          <div className="one wide column">
+          </div>
+          <div className="five wide column">
+            {cards[i + (Math.floor(Math.random() * cards.length))]}
+          </div>
+          <div className="two wide column">
+          </div>
           <div className="two wide column">
           </div>
           <div className="five wide column">
@@ -88,15 +122,15 @@ const Card = React.createClass({
   render: function () {
     return (
       <div id="box">
-        <img src={this.props.id} alt="cover"></img>
+        <img src={this.props.photo} alt="cover"></img>
         <div className="CardContent">
           <h1>{this.props.title}</h1>
           <p>{this.props.project}</p>
         </div>
         <div className="Author">
-          <img src="" alt="author"></img>
-          <div className="name"></div>
-          <div className="time"></div>
+          <img src={this.props.author} alt="author"></img>
+          <div className="name">{this.props.name}</div>
+          <div className="time">{this.props.time}</div>
         </div>
       </div>
     )
