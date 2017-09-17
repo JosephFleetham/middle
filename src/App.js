705c6bc9edbb24aefa8ec96e6c6779f796dcc083
JosephFleetham <joseph.fleetham@gmail.com>
2017-09-17 20:16:59 +0000

    started integrating randomuser api

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import data from "./data.json"
+import cards from "./data.json"
 
 const App = React.createClass({
   getInitialState: function () {
     return {
-      cards: [
-        {
-          title: 'Practice squat',
-          project: 'Gym Chores',
-          photo: "https://unsplash.it/500/200/?random=" + Math.random()
-        },
-        {
-          title: 'Bake squash',
-          project: 'Kitchen Chores',
-          photo: "https://unsplash.it/500/200/?random=" + Math.random()
-        },
-        {
-          title: "do shit",
-          project: "shit shit",
-          photo: "https://unsplash.it/500/200/?random=" + Math.random()
-        },
-        {
-          title: "more shit",
-          project: "tryign to get rid of shit",
-          photo: "https://unsplash.it/500/200/?random=" + +Math.random()
-        }
-      ],
+      cards: [],
+      person: []
     };
+  },
+  componentDidMount: function () {
+    this.updateState();
+    var that = this;
+    var url = 'https://randomuser.me/api/'
+
+    fetch(url)
+    .then(function(response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      that.setState({ person: data.person });
+    });
+
+  },
+  updateState: function () {
+    this.setState({ cards: cards });
   },
   render() {
     return (
@@ -63,50 +63,55 @@ const CardList = React.createClass({
   // this.setState({
   //   data: update(this.state.data, {$splice: [[index, 1]]})
   // }),
+
   render: function () {
     // leanpub-start-insert
     const cards = this.props.cards.map((card) => (
       <Card
         title={card.title}
-        project={card.project}
+        description={card.description}
         photo={card.photo}
+        author={card.author}
+        authorPhoto={card.authorPhoto}
+        time={card.time}
+
       />
-    ));  
+    ));
     return (
       <div className="ui grid">
         <div className="two wide column">
         </div>
         <div className="five wide column">
-          {cards[(Math.floor(Math.random() * cards.length))]}
+          {cards[0]}
         </div>
         <div className="one wide column">
         </div>
         <div className="five wide column">
-          {cards[(Math.floor(Math.random() * cards.length))]}
+          {cards[1]}
         </div>
         <div className="two wide column">
         </div>
         <div className="two wide column">
         </div>
         <div className="five wide column">
-          {cards[(Math.floor(Math.random() * cards.length))]}
+          {cards[2]}
         </div>
         <div className="one wide column">
         </div>
         <div className="five wide column">
-          {cards[(Math.floor(Math.random() * cards.length))]}
+          {cards[3]}
         </div>
         <div className="two wide column">
         </div>
         <div className="two wide column">
         </div>
         <div className="five wide column">
-          {cards[(Math.floor(Math.random() * cards.length))]}
+          {cards[4]}
         </div>
         <div className="one wide column">
         </div>
         <div className="five wide column">
-          {cards[(Math.floor(Math.random() * cards.length))]}
+          {cards[5]}
         </div>
         <div className="two wide column">
         </div>
@@ -123,11 +128,11 @@ const Card = React.createClass({
         <img src={this.props.photo} alt="cover"></img>
         <div className="CardContent">
           <h1>{this.props.title}</h1>
-          <p>{this.props.project}</p>
+          <p>{this.props.description}</p>
         </div>
         <div className="Author">
-          <img src={this.props.author} alt="author"></img>
-          <div className="name">{this.props.name}</div>
+          <img src={this.props.authorPhoto} alt="authorOhoto"></img>
+          <div className="author">{this.props.author}</div>
           <div className="time">{this.props.time}</div>
         </div>
       </div>
